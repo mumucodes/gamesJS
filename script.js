8767a6311892fcdd180e3bb0f176a11a571f0e99
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-05-02 08:54:11 -0700

    dynamic sound working

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
 
+
 let score = 0;
-let gameFrame = 0;
+let gameFrame= 0;
 ctx.font = '50px Georgia';
 
-
-
+//mouse interactivity
 let canvasPosition = canvas.getBoundingClientRect();
+
+
 const mouse = {
     x: canvas.width/2,
     y: canvas.height/2,
@@ -19,12 +21,11 @@ canvas.addEventListener('mousedown', function(event){
     mouse.click = true;
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
-
 });
-
 canvas.addEventListener('mouseup', function(){
     mouse.click = false;
 })
+//player
 class Player {
     constructor(){
         this.x = canvas.width/2;
@@ -40,14 +41,15 @@ class Player {
         const dx = this.x - mouse.x;
         const dy = this.y - mouse.y;
         if(mouse.x != this.x){
-            this.x -= dx/30;
+            this.x -= dx/20;
         }
-        if (mouse.y != this.y){
-            this.y -= dy/30 ;
+        if(mouse.y != this.y){
+            this.y -= dy/20;
         }
     }
     draw(){
-        if (mouse.click){
+        if(mouse.click){
+            ctx.lineWidth = 0.3;
             ctx.beginPath();
             ctx.moveTo(this.x, this.y);
             ctx.lineTo(mouse.x, mouse.y);
@@ -55,49 +57,69 @@ class Player {
         }
         ctx.fillStyle = 'red';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
     }
 }
 const player = new Player();
 
+//bubbles
 const bubblesArray = [];
-class Bubble {
+class Bubble{
     constructor(){
         this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.y = canvas.height + 100;
         this.radius = 50;
         this.speed = Math.random() * 5 + 1;
         this.distance;
+        this.counted = false;
     }
     update(){
         this.y -= this.speed;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        this.distance = Math.sqrt(dx*dx + dy*dy);
     }
     draw(){
-        ctx.fillStyle = 'blue'
+        ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
         ctx.stroke();
     }
 }
-function handleBubbles() {
-    if (gameFrame % 50 == 0){
+function handleBubbles(){
+    if(gameFrame % 50 == 0){
         bubblesArray.push(new Bubble());
     }
     for (let i = 0; i < bubblesArray.length; i++){
         bubblesArray[i].update();
-        bubblesArray[i].draw;
+        bubblesArray[i].draw();
+    }
+    for (let i = 0; i < bubblesArray.length; i++){
+        if(bubblesArray[i].y < 0 - this.radius *2){
+            bubblesArray.splice(i, 1);
+        }
+        if(bubblesArray[i].distance < bubblesArray[i].radius + player.radius){
+            if(!bubblesArray[i].counted){
+                score++;
+                bubblesArray[i].counted = true;
+                bubblesArray.splice(i, 1);
+            }
+            
+        }
     }
 }
-
-function animate() {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+//animation loop
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     handleBubbles();
     player.update();
     player.draw();
+    ctx.fillStyle = 'black';
+    ctx.fillText('score: ' + score, 10, 50);
     gameFrame++;
     requestAnimationFrame(animate);
 }
